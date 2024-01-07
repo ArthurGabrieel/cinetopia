5c7afdc6887301e592cf3ea26b95007f97a3d27f
Arthur Gabriel Lima Gomes <78980894+ArthurGabrieel@users.noreply.github.com>
2024-01-07 17:30:09 -0300

    Initial Commit

---
+++ cinetopia/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  cinetopia
+//
+//  Created by Arthur Gabriel Gomes on 07/01/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
